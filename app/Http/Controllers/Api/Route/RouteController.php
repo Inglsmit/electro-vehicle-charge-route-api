0646b915f99d826093e48edb2ac6708b24ded7f5
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-08-10 00:08:33 +0300

    UPD: #10 Route Controller

---
+++ app/Http/Controllers/Api/Route/RouteController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\Route;
+
+use App\Http\Controllers\Controller;
+use App\Models\Route;
+use Illuminate\Http\Request;
+
+class RouteController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // TODO: validation / Auth
+        try {
+            $route = new Route;
+
+            $route->name = $request->get('name');
+            $route->from = $request->get('from');
+            $route->to = $request->get('to');
+            $route->range = $request->get('range');
+
+            $route->save();
+
+            return response()->json([
+               'message' => 'The route was added succesfully',
+               'route' => $route
+            ]);
+        }catch (\Exception $e){
+            return response()->json(['error' => 'Something went wrong saving the route'], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Route $route)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Route $route)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Route $route)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Route $route)
+    {
+        //
+    }
+}
